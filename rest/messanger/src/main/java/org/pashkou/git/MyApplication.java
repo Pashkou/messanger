1b0be45094a817124b4507c584fe212ffbb887fa
Siarhei Pashkou <pashkou.s@gmail.com>
2018-08-12 17:41:48 +0200

    update

--- rest/messanger/src/main/java/org/pashkou/git/MyApplication.java
+++ rest/messanger/src/main/java/org/pashkou/git/MyApplication.java
@@ -3,10 +3,7 @@ package org.pashkou.git;
 public class MyApplication {
 	public static void main(String[] args) {
 		System.out.println("second commit, take first commit");
-<<<<<<< HEAD
 		System.out.println("implement new feature");
-=======
->>>>>>> branch 'master' of https://github.com/Pashkou/messanger.git
 		System.out.println("change on master");
 	}
 }
