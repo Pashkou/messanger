4e668ea857c3edb50eabd67c70efa88868d11ea9
Siarhei Pashkou <pashkou.s@gmail.com>
2018-08-12 17:28:39 +0200

    master change

--- rest/messanger/src/main/java/org/pashkou/git/MyApplication.java
+++ rest/messanger/src/main/java/org/pashkou/git/MyApplication.java
@@ -3,5 +3,6 @@ package org.pashkou.git;
 public class MyApplication {
 	public static void main(String[] args) {
 		System.out.println("second commit, take first commit");
+		System.out.println("change on master");
 	}
 }
